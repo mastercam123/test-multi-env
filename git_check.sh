dd28cf40497b5b727e864da74d0b681bfe5b56dd
mastercam123 <k4mbingoce@yahoo.com>
2023-06-07 10:27:58 +0200

    Added bash script

--- git_check.sh
+++ git_check.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-if [[ $(git tag) ]]; then
+if [[ $(git describe --tags --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`) ]]; then
     echo "There are previous version"
-    git describe --tags --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`
 else
-    echo "No tags exist in this repository"
+    echo "No tags exist previously in this repository."
+    echo "This is first tag in the repository."
+    echo "The first tag is compared with the first commit in the branch."
+    echo "prev_tag=$(git rev-list --max-parents=0 HEAD)" >> $GITHUB_OUTPUT
 fi
