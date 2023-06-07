8fea2c4e3efd2a2e743c5c912923f0441d7b832c
mastercam123 <k4mbingoce@yahoo.com>
2023-06-07 11:20:29 +0200

    Updated script

--- git_check.sh
+++ git_check.sh
@@ -8,5 +8,6 @@ else
     echo "No tags exist previously in this repository."
     echo "This is first tag in the repository."
     echo "The first tag is compared with the first commit in the branch."
+    echo "This is the first commit SHA: $(git rev-list --max-parents=0 HEAD)"
     echo "prev_tag=$(git rev-list --max-parents=0 HEAD)" >> $GITHUB_OUTPUT
 fi
