155109fba6782ca302298acdd04d48eb10d22dd6
mastercam123 <k4mbingoce@yahoo.com>
2023-06-07 10:04:40 +0200

    First product

--- git_check.sh
+++ git_check.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+set -e
 
+if [[ $(git tag) ]]; then
+    echo "There are previous version"
+    git describe --tags --abbrev=0 --tags `git rev-list --tags --skip=1 --max-count=1`
+else
+    echo "No tags exist in this repository"
+fi
